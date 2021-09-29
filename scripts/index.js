587cd90e85b01039d8afb226102bd923e87277d5
Elena Maslovskaya <elena.maslovski@yandex.ru>
2021-09-29 10:14:09 +0200

    доработка лайков и добавление карточки

--- scripts/index.js
+++ scripts/index.js
@@ -35,16 +35,79 @@ function closeButtonClick(event) {
    }
 }
 
+
+const initialCards = [
+   {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+   },
+   {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+   },
+   {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+   },
+   {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+   },
+   {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+   },
+   {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+   }
+];
+
+const elements = document.querySelector('.elements');
+const templateCard = document.querySelector('.template-card');
+
+/*
+initialCards.forEach((item) => {
+   const templateCard = document.querySelector('.template-card').content;
+   const newElement = templateCard.cloneNode(true);
+
+   newElement.querySelector('.element__image').src = item.link;
+   newElement.querySelector('.element__name').textContent = item.name;
+
+   elements.append(newElement);
+});
+*/
+
+function createCard(item) {
+   const newElement = templateCard.content.cloneNode(true);
+
+   newElement.querySelector('.element__image').src = item.link;
+   newElement.querySelector('.element__name').textContent = item.name;
+
+   //Лайк поставить/удалить
+   newElement.querySelector('.element__like-icon').addEventListener('click', function (evt) {
+      evt.target.classList.toggle('element__like-icon_active');
+   });
+
+   elements.append(newElement);
+
+}
+
+initialCards.map(createCard);
+
 let popupPhoto = document.querySelector('#photo-popup');
 let photoName = document.querySelector('.element__name');
 let photoLink = document.querySelector('.elememt__image');
 let popupPhotoName = document.querySelector('#photo-name');
 let popupPhotoLink = document.querySelector('#link');
 let popupPhotoOpenButton = document.querySelector('.profile__add-button');
-let popupPhotoCloseButton = document.querySelector('#popup-photo-close');
+let popupPhotoCloseButton = document.querySelector
+('#popup-photo-close');
+let popupPhotoForm = document.querySelector('#photo-form');
 
 popupPhotoOpenButton.addEventListener('click', openPhotoForm);
 popupPhotoCloseButton.addEventListener('click', closePhotoForm);
+popupPhotoForm.addEventListener('submit', formSubmitAdd);
 popupPhoto.addEventListener('click', closePhotoButtonClick);
 
 function openPhotoForm() {
@@ -56,15 +119,18 @@ function closePhotoForm() {
    popupPhoto.classList.toggle('popup_opened');
 }
 
-function photoAddHandler(event) {
-   event.preventDefault();
-   photoName.textContent = popupPhotoName.value;
-   photoLink.src = popupPhotoLink.value;
-   closePhotoForm(popupPhotoName.value, popupPhotoLink.value);
-}
-
 function closePhotoButtonClick(event) {
    if (event.target === event.currentTarget) {
       closePhotoForm();
    }
+}
+
+function formSubmitAdd(evt) { // функция добавления карточки
+   evt.preventDefault();
+   const cardTemplate = document.querySelector('.template-card').content;
+   const cardElement = cardTemplate.cloneNode(true);
+   cardElement.querySelector('.element__image').src = popupPhotoLink.value;
+   cardElement.querySelector('.element__name').textContent = popupPhotoName.value;
+   elements.prepend(cardElement);
+   closePhotoForm();
 }
--- ubung.js
+++ ubung.js
@@ -31,3 +31,21 @@ const elementLike = document.querySelector('.element__like-icon');
 elementLike.addEventListener('click', function (evt) {
    evt.target.classList.toggle('element__like-icon_active');
 });
+
+const cards = document.querySelector('.places__cards'); // находим список карточек
+initialCards.forEach((item) => { // обход массива, в функции параметр item - каждый элемент массива
+  const cardTemplate = document.querySelector('.card-template').content; // находим шаблон карточки
+  const cardElement = cardTemplate.cloneNode(true); // клонируем шаблон карточки, параметр true говорит, что мы клонируем вместе с содержимым
+  cardElement.querySelector('.card__image').src = item.link; // в img (находим по классу) вставляем в атрибут src значение ключа link
+  cardElement.querySelector('.card__title').textContent = item.name; // наполняем заголовок h2 (находим по классу) берем значение из ключа name
+  cards.append(cardElement); // добавляем получившийся узел в конец cards
+});
+function formSubmitAdd(evt) { // функция добавления карточки
+  evt.preventDefault();
+  const cardTemplate = document.querySelector('.card-template').content;
+  const cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.card__image').src = placeLinkInput.value;
+  cardElement.querySelector('.card__title').textContent = placeNameInput.value;
+  cards.prepend(cardElement);
+  closePopup(popupAdd);
+}
